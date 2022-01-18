8ac3549bfb315b186d306694f67401c1408b3998
phanlong99 <phanlong99mq@gmail.com>
2022-01-18 10:44:31 +0700

    Updates

---
+++ precache-manifest.efaebffae96f6910cc399ce6f884dff9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c628134aa3d9b8c0f56c3131631d3852",
+    "url": "git@github.com:phanlong99/test_reactjs.git/index.html"
+  },
+  {
+    "revision": "2259d6ca9e20999d0cdd",
+    "url": "git@github.com:phanlong99/test_reactjs.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "31d07cbf58bd49bd4fa3",
+    "url": "git@github.com:phanlong99/test_reactjs.git/static/js/2.7ccfe46a.chunk.js"
+  },
+  {
+    "revision": "ef5ca3a66ce237d1b8ca",
+    "url": "git@github.com:phanlong99/test_reactjs.git/static/js/3.fa6130ed.chunk.js"
+  },
+  {
+    "revision": "2259d6ca9e20999d0cdd",
+    "url": "git@github.com:phanlong99/test_reactjs.git/static/js/main.e5b4f4f5.chunk.js"
+  },
+  {
+    "revision": "fe6f2dc1385b3e3afabe",
+    "url": "git@github.com:phanlong99/test_reactjs.git/static/js/runtime-main.3634794c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "git@github.com:phanlong99/test_reactjs.git/static/media/logo.06e73328.svg"
+  }
+]);
